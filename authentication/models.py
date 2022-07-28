134c870e66f755e0369500010a8fbe79b9a45ab3
sreenandan <sreenandan03@gmail.com>
2022-07-28 18:20:41 +0530

    feature : added details to tokens

--- authentication/models.py
+++ authentication/models.py
@@ -10,8 +10,10 @@ from django.contrib.auth.models import User
 class Tokens(models.Model):
     user = models.OneToOneField(User, related_name='tokens', on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=12, default="")
-
-    # profile = models.ImageField(upload_to='meadia',blank=True,null=True)
+    expertise = models.CharField(max_length=12, default="")
+    occupation = models.CharField(max_length=15, default="")
+    gender = models.CharField(choices=[("male","male")("female","female")("others","others")])
+    profile = models.ImageField(upload_to='media',blank=True,null=True)
 
     def __str__(self):
         return f"{self.user} "
